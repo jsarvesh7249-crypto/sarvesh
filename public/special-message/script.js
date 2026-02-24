e6e96592927be6132bc58da05029aaaf98e6d279
sarvesh <jsarvesh7249@gmail.com>
2026-02-24 17:10:41 +0530

    Remove public pages

--- public/special-message/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('myButton').addEventListener('click', function () {
-    // Hide the button
-    this.style.display = 'none';
-
-    // Show the message
-    document.getElementById('message').classList.remove('hidden');
-});
